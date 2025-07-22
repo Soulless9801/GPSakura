23198bb6284219bf24f8dbcd6c6ffbe29fe24cc6
Soulless9801 <103860106+Soulless9801@users.noreply.github.com>
2025-07-21 21:01:38 -0500

    added latex

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,11 +1,41 @@
+import { InlineMath } from 'react-katex';
+import 'katex/dist/katex.min.css';
+
 import './BlogPost.css'
 
-export default function BlogPost({title, body, time}) {
-  return (
-    <div className="container-fluid blogPostContainer">
-        <div className="blogPostTitle">{title}</div>
-        <div className="blogPostBody">{body}</div>
-        <small className="blogPostTime">Created {time.toDate().toLocaleDateString()}</small>
-    </div>
-  );
+const TextParser = ({ text }) => {
+    const blocks = text.split('\\n');
+    return (
+        <span>
+            {blocks.map((block, index) => {
+                return <LaTeXParser text={block}/>;
+            })}
+        </span>
+    )
+}
+
+const LaTeXParser = ({ text }) => {
+    const parts = text.split(/(\$[^$]*\$)/g);
+    return (
+        <p>
+            {parts.map((part, index) => {
+                if (part.startsWith('$') && part.endsWith('$')) {
+                    const math = part.slice(1, -1);
+                    return <InlineMath key={index} math={math} />;
+                } else {
+                    return <span key={index}>{part}</span>;
+                }
+            })}
+        </p>
+    );
+};
+
+export default function BlogPost({ title, body, time }) {
+    return (
+        <div className="container-fluid blogPostContainer">
+            <div className="blogPostTitle">{title}</div>
+            <div className="blogPostTime">Created {time.toDate().toLocaleDateString()}</div>
+            <div className="blogPostBody"><TextParser text={body} /></div>
+        </div>
+    );
 }
