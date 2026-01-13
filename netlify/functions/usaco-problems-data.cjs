f27cf2e8be1a5c8ac9146dadf83f3fe2aed1632d
Soulless9801 <103860106+Soulless9801@users.noreply.github.com>
2026-01-12 20:06:21 -0600

    start usaco problems

--- netlify/functions/usaco-problems-data.cjs
+++ netlify/functions/usaco-problems-data.cjs
@@ -11,7 +11,6 @@ const fireStore = require('firebase/firestore');
 const { 
     FIREBASE_API_KEY, 
     FIREBASE_AUTH_DOMAIN, 
-    FIREBASE_DATABASE_URL,
     FIREBASE_PROJECT_ID, 
     FIREBASE_STORAGE_BUCKET, 
     FIREBASE_MESSAGING_SENDER_ID, 
@@ -21,8 +20,7 @@ const {
 
 const firebaseConfig = {
     apiKey: FIREBASE_API_KEY, 
-    autoDomain: FIREBASE_AUTH_DOMAIN,
-    databaseURL: FIREBASE_DATABASE_URL, 
+    autoDomain: FIREBASE_AUTH_DOMAIN, 
     projectId: FIREBASE_PROJECT_ID, 
     storageBucket: FIREBASE_STORAGE_BUCKET, 
     messagingSenderId: FIREBASE_MESSAGING_SENDER_ID, 
@@ -31,43 +29,36 @@ const firebaseConfig = {
 };
 
 exports.handler = async (event, context) => {
-    
+
     let firebaseApp;
 
     if (!firebase.getApps().length) firebaseApp = firebase.initializeApp(firebaseConfig);
     else firebaseApp = firebase.getApp();
 
-    let posts;
+    let problems;
 
     try {
 
         const db = fireStore.getFirestore(firebaseApp);
     
-        const q = fireStore.query(fireStore.collection(db, 'posts'));
+        const q = fireStore.query(fireStore.collection(db, 'usaco'));
 
         const snap = await fireStore.getDocs(q);
 
         if (snap.empty) throw new Error();
 
-        posts = snap.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-        posts = posts.map(post => {
-            return {
-                ...post,
-                timestamp: post.timestamp.toDate().toISOString(),
-                updated: post.updated.toDate().toISOString(),
-            };
-        });
+        problems = snap.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 
-        posts = JSON.stringify(posts);
+        problems = JSON.stringify(problems);
 
     } catch (error) {
-        // console.log(`Error: ${error.message}`);
-        const filePath = path.resolve('./netlify/functions/data/firebaseBlogPosts.json');
-        posts = fs.readFileSync(filePath, 'utf8');
+        console.log(`Error: ${error.message}`);
+        const filePath = path.resolve('./netlify/functions/data/firebaseUSACOProblems.json');
+        problems = fs.readFileSync(filePath, 'utf8');
     }
 
     return {
         statusCode: 200,
-        body: posts
+        body: problems
     }
 };
--- src/pages/Coding/USACO.jsx
+++ src/pages/Coding/USACO.jsx
@@ -13,11 +13,14 @@ import Navbar from '/src/components/tools/Navbar/Navbar.jsx'
 
 import PageTitle from '/src/components/tools/PageTitle/PageTitle.jsx'
 
+import USACOTable from '/src/components/tables/USACOTable/USACOTable.jsx'
+
 export default function USACO(){
     return (
         <StrictMode>
             <Navbar />
             <PageTitle title="USACO" description="The cow says moo"/>
+            <USACOTable />
         </StrictMode>
     );
 }
