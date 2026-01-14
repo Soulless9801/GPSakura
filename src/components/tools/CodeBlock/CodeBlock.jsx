132034a70bfc7df93567237dd2ddde8c790a02cd
Soulless9801 <103860106+Soulless9801@users.noreply.github.com>
2026-01-14 00:36:30 -0600

    fixed syntax highlighting issue

--- src/components/tools/CodeBlock/CodeBlock.jsx
+++ src/components/tools/CodeBlock/CodeBlock.jsx
@@ -1,13 +1,8 @@
-import { useEffect } from 'react';
 import { ShikiHighlighter } from 'react-shiki';
 
 import './CodeBlock.css';
 
 export default function CodeBlock({ code, lang="cpp", theme="github-dark" }) {
-
-    useEffect(() => {
-        console.log(code);
-    }, [code]);
 
     return (
         <ShikiHighlighter language={lang} theme={theme} className="code-block">
