87b948793dfaf68a9ef55396d95b373abdf16a04
JBOI <jasonli2026@gmail.com>
2026-04-14 22:46:23 -0500

    3d test

---
+++ src/pages/Experiments/ThreeD.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react'
+
+import '/src/index.css'
+
+import 'jquery'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
+import '@fortawesome/fontawesome-free/css/all.css'
+
+import Navbar from '/src/components/tools/Navbar/Navbar.jsx'
+
+import PageTitle from '/src/components/tools/PageTitle/PageTitle.jsx'
+
+import ThreeD from '/src/components/experiments/ThreeD/ThreeD.jsx'
+
+export default function TriD(){
+    return (
+        <StrictMode>
+            <Navbar />
+            <PageTitle title="3D Playground" description="A space of chance"/>
+            <ThreeD />
+        </StrictMode>
+    )
+}
