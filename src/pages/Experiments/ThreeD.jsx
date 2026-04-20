33e083acf4d0e5c932f4b05d4db163df51ffdb2c
Soulless9801 <103860106+Soulless9801@users.noreply.github.com>
2026-04-20 02:07:38 -0500

    added color selector component

--- src/pages/Experiments/ThreeD.jsx
+++ src/pages/Experiments/ThreeD.jsx
@@ -13,13 +13,16 @@ import Navbar from '/src/components/tools/Navbar/Navbar.jsx'
 
 import PageTitle from '/src/components/tools/PageTitle/PageTitle.jsx'
 
-// import TriD from '/src/components/experiments/ThreeD/ThreeD.jsx'
+import ThreeD from '/src/components/experiments/ThreeD/ThreeD.jsx'
 
 export default function TriD(){
     return (
         <StrictMode>
             <Navbar />
             <PageTitle title="3D Playground" description="A space of chance"/>
+            <div style={{ width: '90vw', height: '90vh', margin: '5vh auto' }}>
+                <ThreeD />
+            </div>
         </StrictMode>
     );
 }
