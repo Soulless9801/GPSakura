8ef85f19f09e297ae72e2302e59bfde3aeb1686c
Soulless9801 <103860106+Soulless9801@users.noreply.github.com>
2026-05-29 02:02:00 -0500

    centralized firebase config initialization

--- netlify/functions/cp-problem-data.js
+++ netlify/functions/cp-problem-data.js
@@ -3,30 +3,9 @@ import fs from 'node:fs';
 import path from 'node:path';
 import * as firebase from 'firebase/app';
 import * as fireStore from 'firebase/firestore';
+import { firebaseConfig } from './firebase-auth.js';
 
 dotenv.config();
-
-const {
-    FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN,
-    FIREBASE_DATABASE_URL,
-    FIREBASE_PROJECT_ID,
-    FIREBASE_STORAGE_BUCKET,
-    FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_APP_ID,
-    FIREBASE_MEASUREMENT_ID,
-} = process.env;
-
-const firebaseConfig = {
-    apiKey: FIREBASE_API_KEY,
-    autoDomain: FIREBASE_AUTH_DOMAIN,
-    databaseURL: FIREBASE_DATABASE_URL,
-    projectId: FIREBASE_PROJECT_ID,
-    storageBucket: FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-    appId: FIREBASE_APP_ID,
-    measurementId: FIREBASE_MEASUREMENT_ID,
-};
 
 export const handler = async (event, context) => {
     const id = event.queryStringParameters.id;
@@ -36,7 +15,7 @@ export const handler = async (event, context) => {
     if (!firebase.getApps().length) firebaseApp = firebase.initializeApp(firebaseConfig);
     else firebaseApp = firebase.getApp();
 
-    let data;
+    let data;   
 
     try {
         const db = fireStore.getFirestore(firebaseApp);
